be2df370818dbf0f973013fc937bb4afb05df733
Cauê Thenório <cauelt@gmail.com>
2012-09-17 12:39:34 -0300

    adicionado o faces-config.xml e script para download automatico das dependências em sistemas *nix

---
+++ code/labori/scripts/download_requirements.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+set +v
+LIBS_DIR=../code/labori/web/WEB-INF/lib
+
+mkdir -p $LIBS_DIR
+cd $LIBS_DIR
+echo Downloading omnifaces...
+curl http://omnifaces.googlecode.com/files/omnifaces-1.1.jar -O
+echo DONE.
