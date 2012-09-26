91a80d7f35917ec40c96c48d690e9415d8718ca4
Cauê Thenório <cauelt@gmail.com>
2012-09-26 04:46:18 -0300

    integrando com o travis - quase lá

--- scripts/build.sh
+++ scripts/build.sh
@@ -6,26 +6,11 @@
 _PWD=`pwd`
 
 CopyLibs=$_PWD/lib/org-netbeans-modules-java-j2seproject-copylibstask.jar
+GeneralLibs=`echo $_PWD/lib/*.jar | tr ' ' ':'`
+HibernateLibs=`echo $_PWD/lib/hibernate-support/*.jar | tr ' ' ':'`
+DerbyClientLib="$_PWD/lib/derbyclient.jar"
 
-HibernateLibs="$_PWD/lib/hibernate-support/antlr-2.7.6.jar"
-HibernateLibs="$HibernateLibs;$_PWD/lib/hibernate-support/asm.jar"
-HibernateLibs="$HibernateLibs;$_PWD/lib/hibernate-support/asm-attrs.jar"
-HibernateLibs="$HibernateLibs;$_PWD/lib/hibernate-support/cglib-2.1.3.jar"
-HibernateLibs="$HibernateLibs;$_PWD/lib/hibernate-support/commons-collections-2.1.1.jar"
-HibernateLibs="$HibernateLibs;$_PWD/lib/hibernate-support/dom4j-1.6.1.jar"
-HibernateLibs="$HibernateLibs;$_PWD/lib/hibernate-support/ehcache-1.2.3.jar"
-HibernateLibs="$HibernateLibs;$_PWD/lib/hibernate-support/jdbc2_0-stdext.jar"
-HibernateLibs="$HibernateLibs;$_PWD/lib/hibernate-support/jta.jar"
-HibernateLibs="$HibernateLibs;$_PWD/lib/hibernate-support/hibernate3.jar"
-HibernateLibs="$HibernateLibs;$_PWD/lib/hibernate-support/hibernate-tools.jar"
-HibernateLibs="$HibernateLibs;$_PWD/lib/hibernate-support/hibernate-annotations.jar"
-HibernateLibs="$HibernateLibs;$_PWD/lib/hibernate-support/hibernate-commons-annotations.jar"
-HibernateLibs="$HibernateLibs;$_PWD/lib/hibernate-support/hibernate-entitymanager.jar"
-HibernateLibs="$HibernateLibs;$_PWD/lib/hibernate-support/javassist.jar"
-
-DerbyClient="$_PWD/lib/derbyclient.jar"
-
-CLASSPATH=$CLASSPATH:$_PWD/lib/ant-contrib-1.0b3.jar:$CopyLibs:$HibernateLibs:$DerbyClient
+CLASSPATH=$CLASSPATH:$_PWD/lib/ant-contrib-1.0b3.jar:$CopyLibs:$GeneralLibs:$HibernateLibs
 export CLASSPATH
 echo "CLASSPATH:"$CLASSPATH
 
@@ -46,7 +31,7 @@ for i in * ; do
     echo "----- $i -----"
     ant -Dlibs.CopyLibs.classpath="$CopyLibs" \
         -Dlibs.hibernate-support.classpath="$HibernateLibs" \
-        -Dfile.reference.derbyclient.jar="$DerbyClient" \
+        -Dfile.reference.derbyclient.jar="$DerbyClientLib" \
         -Dj2ee.server.home="$_PWD" $TARGET
     RET=$?
     if [ ! "$RET" = "0" ]; then
