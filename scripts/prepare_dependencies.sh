9e1f4648d1c8a0c3f14ccf51377ea8d94f40d089
Cauê Thenório <cauelt@gmail.com>
2012-09-26 03:26:17 -0300

    integrando com o travis - testes

--- scripts/prepare_dependencies.sh
+++ scripts/prepare_dependencies.sh
@@ -5,8 +5,15 @@ _PWD=`pwd`
 LIBS_DIR=$_PWD/lib
 mkdir -p $LIBS_DIR
 
+echo ==============
+echo Downloading dependencies package...
 wget https://dl.dropbox.com/u/12910711/labori-libs.tar.gz -P $LIBS_DIR/
 tar xzvf $LIBS_DIR/labori-libs.tar.gz -C $LIBS_DIR
+
+echo ==============
+echo Downloading Hibernate 3.2.5...
+wget http://superb-dca2.dl.sourceforge.net/project/hibernate/hibernate3/3.2.5.ga/hibernate-3.2.5.ga.tar.gz -P $LIBS_DIR
+tar xzvf $LIBS_DIR/hibernate-3.2.5.ga.tar.gz -C $LIBS_DIR
 
 # LIBS_DIR=../web/WEB-INF/lib
 
