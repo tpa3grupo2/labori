5dc08c16a1c13d6110902be624ac8d0e17013047
Cauê Thenório <cauelt@gmail.com>
2012-09-26 02:30:24 -0300

    Início da integração com o Travis-CI

---
+++ scripts/build.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+# download .jar dependencies
+#./prepare_dependencies.sh
+
+cd ..
+_PWD=`pwd`
+
+CopyLibs=$_PWD/lib/org-netbeans-modules-java-j2seproject-copylibstask.jar
+
+CLASSPATH=$CLASSPATH:$_PWD/lib/ant-contrib-1.0b3.jar:$CopyLibs
+export CLASSPATH
+echo "CLASSPATH:"$CLASSPATH
+
+echo " "
+echo "Compiling these projects:"
+for i in * ; do
+  if [ -d "$i" ] && [ ! "$i" = "lib" ]; then
+    cd "$i"
+
+    if [ -f "manifest.mf" ]; then
+        TARGET=jar
+    else
+        TARGET=dist
+    fi
+    TARGET=""
+
+    echo " "
+    echo "----- $i -----"
+    ant -Dlibs.CopyLibs.classpath="$CopyLibs" \
+        -Dj2ee.server.home="$_PWD" $TARGET
+    RET=$?
+    if [ ! "$RET" = "0" ]; then
+        exit $RET
+    fi
+    echo " "
+    cd ..
+  fi
+done
+
